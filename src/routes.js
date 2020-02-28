70075f46c9e330ea4d9d273e53b51083b8d25c53
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-02-28 13:42:58 -0300

    rotas de  navegação

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: '#00869b',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
